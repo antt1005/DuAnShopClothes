ed42f54af211f9f09081954b008e87ef4798a9f5
Duybv6703 <duybv6703@gmail.com>
2024-12-03 21:40:57 +0700

    load data repo color

--- src/main/java/com/example/demo/repositories/ColorRepo.java
+++ src/main/java/com/example/demo/repositories/ColorRepo.java
@@ -2,9 +2,23 @@ package com.example.demo.repositories;
 
 
 import com.example.demo.entity.Color;
+import com.example.demo.entity.propertis.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ColorRepo extends JpaRepository<Color, Long> {
+
+
+    @Query(value = "select c from Color c where c.status = 'DANG_HOAT_DONG'")
+    List<Color> getAll();
+
+    Page<Color> getAllByStatus(Status status, Pageable pageable);
+
+
 }
